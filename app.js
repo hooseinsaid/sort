281ad9e900288466e19e06d60a6f094689e0865b
hooseinsaid <hussainisaidadam@gmail.com>
2017-11-23 18:41:54 +0300

    listening on a server

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World!');
+}).listen(8090);
 var msg = 'Hello World';
 console.log(msg);
+console.log("what the fuck is this ?");
